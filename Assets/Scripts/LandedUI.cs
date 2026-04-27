c52793d3f5bb8000069761d13a603d0ee0d71081
Manasha Koirala <kmanasha22@tbc.edu.np>
2026-04-27 14:41:46 +0545

    Added dynamic score display to the scoreboard

--- Assets/Scripts/LandedUI.cs
+++ Assets/Scripts/LandedUI.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 public class LandedUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI titleTextMesh;
-    [SerializeField] private TextMeshProUGUI statsTextMesh;
+    [SerializeField] private TextMeshProUGUI statsTextMesh;\
+
+    private void Start()
+    {
+        Lander.Instance.OnLanded += Lander_OnLanded;
+    }
+
+    private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e)
+    {
+        if (e.landingType == Lander.LandingType.Success)
+        {
+            titleTextMesh.text = "CONGRATS SUCCESSFUL LANDING!";
+        } else
+        {
+            titleTextMesh.text = "OOPS CRASHED!";
+        }
+
+        statsTextMesh.text =
+            Mathf.Round(e.landingSpeed) + "\n" +
+            Mathf.Round(e.dotVector) + "\n" +
+            "x" + e.scoreMultiplier + "\n" +
+            e.score;
+
+    }
 }
