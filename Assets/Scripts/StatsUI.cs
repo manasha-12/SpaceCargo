4acc0f56b70a62f50e036f1b992614889c0dfdcf
Manasha Koirala <kmanasha22@tbc.edu.np>
2026-04-30 15:13:09 +0545

    Color indication for fuel consuption

--- Assets/Scripts/StatsUI.cs
+++ Assets/Scripts/StatsUI.cs
@@ -12,15 +12,27 @@ public class StatsUI : MonoBehaviour
     {
         UpdateStatsTextMesh();
     }
+
     private void UpdateStatsTextMesh()
     {
-        fuelImage.fillAmount = Lander.Instance.GetFuelAmountNormalized();
+        if (Lander.Instance == null || GameManager.Instance == null) return;
+
+        float fuelPercent = Lander.Instance.GetFuelAmountNormalized();
+        fuelImage.fillAmount = fuelPercent;
+
+        // Dynamic color based on fuel level
+        if (fuelPercent < 0.25f)
+            fuelImage.color = Color.red; // Critical!
+        else if (fuelPercent < 0.5f)
+            fuelImage.color = Color.yellow; // Warning
+        else
+            fuelImage.color = Color.green; // Good
+
         statsTextMesh.text =
             GameManager.Instance.GetScore() + "\n" +
             Mathf.Round(GameManager.Instance.GetTime()) + "\n" +
             Mathf.Round(Lander.Instance.GetSpeedX() * 10f) + "\n" +
             Mathf.Round(Lander.Instance.GetSpeedY() * 10f);
         levelTextMesh.text = GameManager.Instance.GetLevelNumber().ToString();
-
     }
 }
