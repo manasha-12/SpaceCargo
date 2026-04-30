a8faaa06036871b6af33267a019cfdf81f91ce0b
Manasha Koirala <kmanasha22@tbc.edu.np>
2026-04-30 17:41:24 +0545

    Added coin pickup sound

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] private ParticleSystem collectionEffect;
+    [SerializeField] private AudioClip collectionSound; // Add this
 
     public void DestroySelf()
     {
-        // Spawn particle effect before destroying
+        // Spawn particle effect
         if (collectionEffect != null)
         {
             Instantiate(collectionEffect, transform.position, Quaternion.identity);
+        }
+
+        // Play sound effect
+        if (collectionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(collectionSound, transform.position);
         }
 
         Destroy(gameObject);
